a5cf5aea2b124c6792ef029ca82ea66e09bc8ad6
ksar79 <kevinseiti@hotmail.com>
2020-09-22 14:24:23 -0300

    Add files via upload
    Criando esqueleto dos step definitions de "adicionar_tarefa"

---
+++ features/step_definitions/adicionar_tarefa_steps.rb
@@ -0,0 +1,51 @@
+Dado('que estou na página de registro de tarefas') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('clico em adicionar') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('preencho o campo {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('clico e salvar') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('ele deve ter sido salvo no banco de dados') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deverei ver a tarefa no registro de tarefas') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('deixo o campo {string} vazio') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('clico em salvar') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deverei ver a mensagem de erro {string}') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('preencho o campo data') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('coloco uma data anterior a data atual') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('envio um arquivo anexo') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando('o arquivo tem tamanho maior que 50MB') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
