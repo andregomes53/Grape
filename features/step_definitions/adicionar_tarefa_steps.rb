247d5365cbc3acd7fd63f5f4fc7a321247ac4e5f
Andre Gomes <andrecos8787@usp.br>
2020-09-23 11:08:56 -0300

    preenchendo o arquivo adicionar_tarefa_steps.rb
    Co-Authored-By: ksar79 <kevinseiti@hotmail.com>

--- features/step_definitions/adicionar_tarefa_steps.rb
+++ features/step_definitions/adicionar_tarefa_steps.rb
@@ -3,31 +3,32 @@ Dado('que estou na página de registro de tarefas') do
 end
 
 Quando('preencho o campo {string} com {string}') do |string, string2|
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in string, :with => string2
 end
 
 Quando('clico em adicionar') do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_on 'Adicionar nova tarefa'
 end
 
 Então('ele deve ter sido salvo no banco de dados') do
-  pending # Write code here that turns the phrase above into concrete actions
+  tarefa = Tarefa.order("id").last
+  expect(tarefa.titulo).to eq('Nova tarefa')
 end
 
 Então('deverei ver a tarefa no registro de tarefas') do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content('Nova tarefa')
 end
 
 Quando('deixo o campo {string} vazio') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in string, :with => ""
 end
 
 Então('deverei ver a mensagem de erro {string}') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content(string)
 end
 
 Quando('preencho o campo {string} com uma data anterior a data atual') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content(string)
 end
 
 Quando('anexo um arquivo de tamanho maior que 50MB') do
