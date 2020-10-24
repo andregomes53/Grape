2089f0496a88fa613b4ae078dae5aec0926f391e
ksar79 <kevinseiti@hotmail.com>
2020-10-24 19:08:26 -0300

    criado o teste para criação de usuário

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+	def new
+	end
+
+	def create
+		User.create(user_params)
+	end
+
 	def show
+	end
+	
+	private
+	def user_params
+		params.require(:user).permit(:name)
 	end
 end
--- spec/users_controller_spec.rb
+++ spec/users_controller_spec.rb
@@ -4,6 +4,17 @@ describe 'UsersController', :type => :controller do
 	before(:context) do
 		@controller = UsersController.new
 	end
+	
+	describe 'creating user' do
+		it 'redirects to the sign up page' do
+			get 'new'
+			expect(response).to render_template(:new)
+		end
+		it 'calls the method that creates a user' do
+			post 'create', params: {user: {name: "Maria"}}
+			expect(response).to be_successful
+		end
+	end
 
 	describe 'showing user' do
 		it 'calls the method that shows the user page' do
@@ -11,7 +22,7 @@ describe 'UsersController', :type => :controller do
 			expect(response).to be_successful
 		end
 		it 'should render the page' do
-		
+			expect(response.body).to include("fazer tarefa de matematica")
 		end
 	end
 end
