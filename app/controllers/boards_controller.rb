9d4bbb89ffc87349cea40bc6d88b1c5c0b5a28ef
Leonardo Britto <leostb@hotmail.com>
2020-10-25 01:01:33 -0300

    Criar view relacionadas a criação de board

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,1 +1,11 @@
+class BoardsController < ApplicationController
+  def show
+  end
 
+  def new
+    @board = Board.new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :tasks
+  resources :boards
   root to: 'pages#home'
 end
