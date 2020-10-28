15cab1123787c30271732d7b7518e2bb8888c55e
Guilherme Senna <guisenna23@hotmail.com>
2020-10-27 23:12:38 -0300

    Arrumar testes

--- features/step_definitions/remover_tarefa_steps.rb
+++ features/step_definitions/remover_tarefa_steps.rb
@@ -3,15 +3,14 @@ Before('@tarefa_duble') do
   @task = Task.new(id:10,title:"Cucumber",deadline:"Ontem",user_id:10)
   @task_2 = Task.new(id:11,title:"Tarefa2",deadline:"Hoje",user_id:10)
   @task.save
-  @task_2.save
-  
+  @task_2.save  
 end
 
 Dado('que estou na página de listagem de tarefas') do
   visit 'tasks'
 end
 
-Quando('clico no remover de uma tarefa') do 
+Quando('clico no remover de uma tarefa') do
   click_on('Remover Tarefa', match: :first)
 end
 
@@ -20,5 +19,5 @@ Então('ela deve ter sido excluída no banco de dados') do
 end
 
 Então('não deverei vê-la no registro de tarefas') do
-  expect(page).not_to have_content("Cucumber")
+  expect(page).not_to have_content("fazer tarefa de matematica")
 end
