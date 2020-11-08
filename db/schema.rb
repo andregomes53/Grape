9356b0502fd62ec4cd6ad0f4e8423cdd7ac2ee99
André Gomes <andrecos8787@usp.br>
2020-11-08 19:20:57 -0300

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_25_100951) do
+ActiveRecord::Schema.define(version: 2020_11_08_203443) do
 
   create_table "boards", force: :cascade do |t|
     t.string "name"
